95e92cda2e521cfc2f700cbc2301c208a6e7400a
Yurii Martynenko <ymartinenko4@gmail.com>
2019-08-23 13:13:38 +0300

    Add new task 4

---
+++ js/fans.js
@@ -0,0 +1,44 @@
+const buttonSend = document.querySelector('.btn-send');
+const listFans = document.querySelector('.list-fans');
+const userName = document.querySelector('.name-comment');
+const userComment = document.querySelector('.text-comment');
+const inputError = document.querySelectorAll('.input_error');
+const alertSuccess = document.querySelector('.alert-success');
+const alertError = document.querySelector('.alert-danger');
+
+function functionName(event) {
+    event.preventDefault();
+
+    const date = new Date();
+    const userNameValue = userName.value;
+    const userCommentValue = userComment.value;
+    if (userNameValue.length < 2 || userCommentValue.length < 6) {
+        alertError.style.display = 'block';
+        setTimeout(function () {
+            alertError.style.display = 'none';
+        }, 3000);
+        return false;
+    }
+
+    alertSuccess.style.display = 'block';
+    setTimeout(function () {
+        alertSuccess.style.display = 'none';
+    }, 3000);
+
+    userName.value = '';
+    userComment.value = '';
+    listFans.insertAdjacentHTML('beforeend',
+        `<hr>
+        <article>
+            <p>
+                ${userCommentValue}
+            </p>
+            <div class="footer-fans">
+                <span>${date.toLocaleString()}</span>
+                <span class="name-fan">${userNameValue}</span>
+            </div>
+        </article>`
+    );
+}
+
+buttonSend.addEventListener('click', functionName);
